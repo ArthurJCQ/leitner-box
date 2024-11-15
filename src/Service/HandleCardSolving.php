a3ec68fc41db84263c3f496075e2ad27d1fd7408
ArthurJCQ <ajacquemin@eleven-labs.com>
2024-11-15 18:20:39 +0100

    Demo jetbrains IA

--- src/Service/HandleCardSolving.php
+++ src/Service/HandleCardSolving.php
@@ -8,23 +8,29 @@ use App\Entity\Card;
 
 readonly class HandleCardSolving
 {
+    // Test delay = number of days between 2 tests
     public const array TEST_DELAY = [1, 3, 7, 15, 30, 60];
 
     public function execute(Card $card, string $answer): bool
     {
+        // Prevent from false-negatives
         $formattedAnswer = strtolower(trim($answer));
 
         // Card solving failed
         if (strtolower($card->getAnswer()) !== $formattedAnswer) {
+            // Reset test delay
             $card->setInitialTestDate(new \DateTime())
-                ->setDelay(1);
+                ->setDelay(self::TEST_DELAY[0]);
 
             return false;
         }
 
         // Card solving passed
+
+        // Array key of the next test delay
         $nextDelayKey = array_search($card->getDelay(), self::TEST_DELAY, true) + 1;
 
+        // No more test delay available: disable the Card
         if (!isset(self::TEST_DELAY[$nextDelayKey])) {
             $card->setInitialTestDate(null)
                 ->setDelay(0)
@@ -33,6 +39,7 @@ readonly class HandleCardSolving
             return true;
         }
 
+        // Next test delay available, update Card's next test delay
         $card->setDelay(self::TEST_DELAY[$nextDelayKey]);
 
         return true;
